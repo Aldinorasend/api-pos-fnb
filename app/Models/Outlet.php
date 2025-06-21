20b219bddf77c7befa8b065a3130cbbb12962f03
nabielascar <ascarnabiel@gmail.com>
2025-06-21 17:53:19 +0700

    Add soft delete discount, user, dan outlet

--- app/Models/Outlet.php
+++ app/Models/Outlet.php
@@ -5,10 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Outlet extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $guarded = ['id'];
     public $incrementing = false;
     protected $table = 'outlet';
